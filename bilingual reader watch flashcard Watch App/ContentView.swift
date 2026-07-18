21e79945569dcfb34e47c9c5b38b509d9673b69c
Akram Rasikh <akram.rasikh@gmail.com>
2026-07-18 21:34:57 +0100

    Initial Commit

---
+++ bilingual reader watch flashcard Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bilingual reader watch flashcard Watch App
+//
+//  Created by Akram Rasikh on 18/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bilingual reader watch flashcard Watch App/bilingual_reader_watch_flashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  bilingual_reader_watch_flashcardApp.swift
+//  bilingual reader watch flashcard Watch App
+//
+//  Created by Akram Rasikh on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct bilingual_reader_watch_flashcard_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
